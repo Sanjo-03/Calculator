64dc2f22ed165ae09c711294c03e1111f124cbc8
sanjo-03 <sanjosunny2003@gmail.com>
2024-12-18 13:53:34 +0530

    message

--- app/src/main/java/com/example/newcalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/newcalculatorapp/MainActivity.java
@@ -41,5 +41,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),MulActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
